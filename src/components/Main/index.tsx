48a43b427195ad2e8a44150ba89ad44fb7f1fb86
Fernando Futila <fernando.futila@gmail.com>
2021-09-16 23:09:33 +0100

    Initialized the application, created Layout, Main, ProfilePage Components and added some styles

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import ProfilePage from '../ProfilePage';
+
+import { 
+    Container, 
+    Header, BackIcon, 
+    ProfileInfo, 
+    BottomMenu, 
+    HomeIcon, 
+    SearchIcon, 
+    BellIcon,
+    EmailIcon
+ } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+        <Header>
+            <button>
+                <BackIcon></BackIcon>
+            </button>
+
+            <ProfileInfo>
+                <strong>Fernando Futila</strong>
+                <span>612 Tweets</span>
+            </ProfileInfo>
+        </Header>
+
+        <ProfilePage/> 
+
+        <BottomMenu>
+            <HomeIcon className="active"/>
+            <SearchIcon/>
+            <BellIcon/>
+            <EmailIcon/>
+        </BottomMenu>
+    </Container>
+ )
+}
+
+export default Main;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { 
+    Container,
+    Banner,
+    Avatar,
+    ProfileData, 
+    LocationIcon,
+    CakeIcon, 
+    Followage,
+    EditButton,
+ } from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+        <Banner>
+            <Avatar/>
+        </Banner>
+
+        <ProfileData>
+            <EditButton outlined>Editar Perfil</EditButton>
+
+            <h1>Fernando Futila</h1>
+            <h2>@fernando_futila</h2>
+
+            <p>
+                Developer at <a href="https://rocketseat.com.br">@Rockeseat</a>
+
+            </p>
+
+            <ul>
+                <li>
+                    <LocationIcon/>
+                    Luanda, Angola
+                </li>
+                <li>
+                    <CakeIcon/>
+                    Nascido(a) em Março de 1999
+                </li>
+            </ul>
+
+            <Followage>
+                <span>
+                    seguindo <strong>94</strong>
+                </span>
+                <span>
+                    <strong>94</strong> seguidores
+                </span>
+            </Followage>
+        </ProfileData>
+    </Container>
+ )
+}
+
+export default ProfilePage;
